aee3557db751f74b7283cf7c516c68fc50c61ae0
Faruk Birkan Uzun <farukbirkanuzun@gmail.com>
2021-01-18 16:14:40 +0300

    ConsoleDataApp
    Console SQL

---
+++ CallStatusUpdate/Connection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallStatusUpdate
+{
+
+    class Connection
+    {
+        static SqlCommand komut;
+        static SqlDataReader reader;
+        public void Conn()
+        {
+             var datasource = @"Server"; //Server İsmi
+
+           
+            var database = "VDB"; //Database İsmi
+
+            var username = "sa"; //SQL Connection sa
+
+           
+
+            var password = "s"; // sa ya ait password
+                                
+            //connection string 
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
+            
+            SqlConnection conn = new SqlConnection(connString);
+
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+              
+
+                Console.WriteLine("Connection successful!");
+                StringBuilder strBuilder = new StringBuilder();
+
+                int sayac = 0;
+                Console.WriteLine("DialId :");
+                string DialId = Console.ReadLine();
+
+                komut = new SqlCommand();
+                komut.Connection = conn;
+                komut.CommandText = "SELECT InsertDate,DialId,phone1 FROM CallList  WHERE DialId like '%" + DialId + "%' ";
+                conn.Open();
+                reader = komut.ExecuteReader();
+               
+
+                while (reader.Read())
+                {
+                    sayac++;
+
+                    double timestamp = Convert.ToDouble(reader[0]);
+                    DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    start = start.AddSeconds(timestamp);
+
+                    Console.WriteLine("InsertDate:" + start + "  -  " + "DialId :  " + reader[1] + "   -  " + "    Telefon No  : " + reader[2] + "\n");
+
+                    
+                    string fileName = @"F:\Logs.txt";
+
+
+
+                    string writeText = "InsertDate:" + start + "  -  " + "DialId :  " + reader[1] + "   -  " + "    Telefon No  : " + reader[2] + "\n"+" Sorgu Tarihi : "+DateTime.Now+"\n";
+
+                    FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                    fs.Close();
+                    File.AppendAllText(fileName, Environment.NewLine + writeText);
+
+
+                }
+                conn.Close();
+
+                Console.WriteLine("Sonuç  :" + sayac + " Data ");
+                
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+        }
+
+    }
+}
---
+++ CallStatusUpdate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallStatusUpdate
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection ...");
+            Connection cn = new Connection();
+            Console.Write("DialId ' ye Göre Aramak için 1 Yazınız.");
+            string a = Console.ReadLine();
+            if (a=="1")
+            {
+                cn.Conn();
+            }
+            else
+            {
+                Console.WriteLine("test");
+            }
+            
+          
+            Console.Read();
+        }
+    }
+}
+
